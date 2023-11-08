3522c2fe123e7877505f8bc39d6bf08037ad7dd6
dverves <denver1033@gmail.com>
2023-11-08 19:26:02 +0200

    added comments

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,8 +13,10 @@ import (
 const Port = ":8080"
 
 func main() {
+	// In the main function, an instance of the config.AppConfig struct is created and stored in the app variable.
 	var app config.AppConfig
 
+	// The script attempts to create a template cache using renderer.CreateTemplateCache() and stores it in the app.TemplateCache.
 	tc, err := renderer.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache", err)
@@ -23,10 +25,17 @@ func main() {
 	//this var to set use cache true or false, when in Dev mode
 	app.UseCache = false
 
+	// The handler.NewRepo(&app) call creates a new repository (*Repository)
+	// and passes the app configuration to it. This repository is responsible for holding application configuration and handling requests.
+	// The NewRepo function in the handler package is used to create a new repository.
 	repo := handler.NewRepo(&app)
+
+	// The handler.NewHandlers(repo) function is called to set the global Repo variable to
+	// the newly created repository. This allows other parts of the code to access the repository.
 	handler.NewHandlers(repo)
 
-	//passing app variable to the render package
+	//The renderer.NewTemplate(&app) function is called to set up
+	// templates with the app configuration. This prepares the application to render HTML templates.
 	renderer.NewTemplate(&app)
 
 	http.HandleFunc("/", handler.Repo.MainHandler)
